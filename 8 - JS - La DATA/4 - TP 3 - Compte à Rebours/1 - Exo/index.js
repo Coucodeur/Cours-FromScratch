35ab0834b648f611721c67dacc0aa875df4dfdca
Coucodeur <matcou68@gmail.com>
2023-06-18 18:03:46 +0200

    on est sur la bonne piste pour le chronometre

--- 8 - JS - La DATA/4 - TP 3 - Compte à Rebours/1 - Exo/index.js
+++ 8 - JS - La DATA/4 - TP 3 - Compte à Rebours/1 - Exo/index.js
@@ -1,6 +1,6 @@
 // Créer la logique de compte à rebours
-
 // // choice.value = 5 minutes
+
 
 // choice.value * 60 = totalSeconds
 
@@ -8,16 +8,46 @@
 // let seconds = totalSeconds % 60
 
 // Créer un événement à la validation du form pour lancer le compte à rebours
+  
+// const start = document.getElementById("start");
+// const countdownDisplay = document.getElementById('countdownDisplay')
+// start.addEventListener("click", (e) => {
+//   e.preventDefault();
+//   let totalSeconds = choice.value * 60;
+//   let minutes = Math.floor(totalSeconds / 60);
+//   let seconds = totalSeconds % 60;
+//   let chrono = [minutes + " : " + seconds];
+//   if (seconds = 0) {
+//     minutes --
+//     seconds = 59
+//   }
+// });
+let seconds = 10
+  countdownDisplay.textContent = seconds
 
-const start = document.getElementById("start");
-console.log(start);
-start.addEventListener("click", (e) => {
-  e.preventDefault();
-  console.log("le btn");
-  let totalSeconds = choice.value * 60;
-  let minutes = Math.floor(totalSeconds / 60);
-  let seconds = totalSeconds % 60;
-  console.log(totalSeconds);
-  console.log(minutes);
-  console.log(seconds);
-});
+const secondsEnMoins = () => {
+  if (seconds >= 1) {
+    setInterval(() => {
+      seconds --;
+      countdownDisplay.textContent = seconds;
+    }, 1000)  
+  }
+  }
+// secondsEnMoins();
+
+const secondsEnMoins2 = () => {
+    if (seconds > 0) {
+      setInterval(() => {
+        seconds --;
+        countdownDisplay.textContent = seconds;
+      }, 1000);
+    } 
+    if (seconds = 0) {
+      seconds = 59;
+    }
+  }
+secondsEnMoins2()
+
+// il vaudrait mieux utiliser setTimeout() plutot
+// que setInterval() et rappeler la fonction de --
+// dans cette meme boucle
