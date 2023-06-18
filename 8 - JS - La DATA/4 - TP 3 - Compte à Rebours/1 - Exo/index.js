97351fb1f19f9e006f2ad4b31e2a9a4b0bee33b0
Coucodeur <matcou68@gmail.com>
2023-06-18 15:24:58 +0200

    pour continuer sur le mac

--- 8 - JS - La DATA/4 - TP 3 - Compte à Rebours/1 - Exo/index.js
+++ 8 - JS - La DATA/4 - TP 3 - Compte à Rebours/1 - Exo/index.js
@@ -9,9 +9,15 @@
 
 // Créer un événement à la validation du form pour lancer le compte à rebours
 
-choice = document.getElementById("choice");
-choice.addEventListener("submit", (e) => {
+const start = document.getElementById("start");
+console.log(start);
+start.addEventListener("click", (e) => {
   e.preventDefault();
-  let minutes = choice.value;
+  console.log("le btn");
+  let totalSeconds = choice.value * 60;
+  let minutes = Math.floor(totalSeconds / 60);
+  let seconds = totalSeconds % 60;
+  console.log(totalSeconds);
   console.log(minutes);
+  console.log(seconds);
 });
